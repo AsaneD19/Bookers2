bac230c336868021e97ae59a7a8b847eef1a9c24
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-03-26 23:48:48 +0000

    on going 05

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+
+
   def index
   end
 
@@ -6,5 +8,20 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render user_path(current_user)
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
